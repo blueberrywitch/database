2031597b23c68bbf8a5468e13a870bde125388a8
DinaraM <blueberrywitch@yandex.ru>
2024-12-06 20:04:01 +0300

    first push

--- src/main/java/dika/database/repository/UserRepository.java
+++ src/main/java/dika/database/repository/UserRepository.java
@@ -4,5 +4,5 @@ import dika.database.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface    UserRepository extends JpaRepository<User,Long>, JpaSpecificationExecutor<User> {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 }
